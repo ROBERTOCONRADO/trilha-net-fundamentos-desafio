eb5df145104efb949f1a28b9309970b6916f21b6
Roberto Conrado <robertoconradorc@gmail.com>
2023-10-12 17:11:23 -0300

    New solution

---
+++ VerificacaoDadosTeste/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VerificacaoDadosTeste {
+  class Program {
+    static void Main(string[] args) {
+      int idTeste;
+      if (int.TryParse(Console.ReadLine(), out idTeste)) {
+        string descricaoTeste = Console.ReadLine();
+        if (idTeste is int && descricaoTeste.Length < 50) 
+        {
+          Console.WriteLine("ID de teste valido.");
+          Console.WriteLine("Descricao valida.");
+        } 
+        else if (descricaoTeste.Length >= 50) 
+        {
+          Console.WriteLine("ID de teste invalido ou descricao muito longa.");
+        } 
+      }
+      else 
+      {
+        Console.WriteLine("ID de teste invalido.");
+        Console.ReadLine();
+      }
+    }
+  }
+}

a87ffcdd86be805ff4fc8dc589846043238b62ba
Roberto Conrado <robertoconradorc@gmail.com>
2023-10-13 14:49:44 -0300

    explorando o C#

--- Explorando-C#/Models/Pessoa.cs
+++ Explorando-C#/Models/Pessoa.cs
@@ -7,6 +7,15 @@ namespace Explorando_C_.Models
 {
     public class Pessoa
     {
+        public Pessoa()
+        {
+
+        }
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
         private string _nome;
         private int _idade;
         public string Nome
--- Explorando-C#/Program.cs
+++ Explorando-C#/Program.cs
@@ -1,7 +1,31 @@
 ﻿using Explorando_C_.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Roberto";
-p1.Sobrenome = "Conrado";
-p1.Idade = 29;
-p1.Apresentar();
+Pessoa p1 = new Pessoa(nome: "Roberto", sobrenome: "Conrado");
+Pessoa p2 = new Pessoa(nome: "Jose", sobrenome: "Rocha");
+
+Curso cursoIngles = new Curso();
+cursoIngles.Nome = "Ingles";
+cursoIngles.Alunos = new List<Pessoa>();
+
+cursoIngles.AdicionarAluno(p1);
+cursoIngles.AdicionarAluno(p2);
+cursoIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Roberto";
+// p1.Sobrenome = "Conrado";
+// p1.Idade = 29;
+// p1.Apresentar();

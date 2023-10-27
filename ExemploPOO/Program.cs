1a3c0e5a2551716b90c651c00f9db3dae847e06f
Roberto Conrado <robertoconradorc@gmail.com>
2023-10-27 15:08:45 -0300

    Herança

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -1,22 +1,21 @@
 ﻿using ExemploPOO.Models;
 
-Contacorrente c1 = new Contacorrente(123, 1000);
-
-c1.ExibirSaldo();
-c1.Sacar(1001);
-c1.ExibirSaldo();
-
-
-
+Aluno a1 = new Aluno();
+a1.Nome = "Beto";
+a1.Idade = 29;
+a1.Email = "robertoconradorc@gmail.com";
+a1.Nota = 7.2;
+a1.Apresentar();
 
 
 
 
 
+// Contacorrente c1 = new Contacorrente(123, 1000);
 
-
-
-
+// c1.ExibirSaldo();
+// c1.Sacar(1001);
+// c1.ExibirSaldo();
 
 
 
